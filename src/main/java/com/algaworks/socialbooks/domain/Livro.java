28bb23911ae34dd0733c664ffa0b1181320cd39c
Vanessa R. Alves <vanessah91@hotmail.com>
2021-12-16 20:19:35 -0300

    modelando primeiro recurso e criando a representação

---
+++ src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -0,0 +1,67 @@
+package com.algaworks.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class Livro {
+
+	private Long id;
+	private String nome;
+	private Date publicacao;
+	private String editora;
+	private String resumo;
+	private List<Comentario> comentarios;
+	private String autor;
+	
+	public Livro() {}
+	
+	public Livro(String nome) {
+		this.nome = nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getPublicacao() {
+		return publicacao;
+	}
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getResumo() {
+		return resumo;
+	}
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,26 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Livro;
+
+@RestController
+public class LivrosResources {
+	
+	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	public List<Livro> listar() {
+		
+		Livro l1 = new Livro("REST Aplicado");
+		Livro l2 = new Livro("Git passo-a-passo");
+		
+		Livro[] livros = {l1, l2};
+		
+		return Arrays.asList(livros);
+	}
+
+}
