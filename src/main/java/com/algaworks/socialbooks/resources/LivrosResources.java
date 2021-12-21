029e3207c7cac1fc07226e65a871800d15337064
vanessav02 <vanessah91@hotmail.com>
2021-12-21 08:20:04 -0300

    métodos DELETE e PUT

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,8 +1,10 @@
 package com.algaworks.socialbooks.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,4 +30,19 @@ public class LivrosResources {
 		livrosRepository.save(livro);
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Livro> buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public Livro atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+		livro.setId(id);
+		return livrosRepository.save(livro);
+	}
 }
